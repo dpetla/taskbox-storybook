f7bffa9404ae0c8a6c94893ccab32b32f9f1a11f
Douglas Petla <douglas.petla@rangle.io>
2019-10-11 14:12:06 -0400

    storybook: data

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { actions, task } from './Task.stories';
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 
 export const defaultTasks = [
   { ...task, id: '1', title: 'Task 1' },
@@ -19,7 +19,7 @@ export const withPinnedTasks = [
 
 storiesOf('TaskList', module)
   .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
-  .add('default', () => <TaskList tasks={defaultTasks} {...actions} />)
-  .add('withPinnedTasks', () => <TaskList tasks={withPinnedTasks} {...actions} />)
-  .add('loading', () => <TaskList loading tasks={[]} {...actions} />)
-  .add('empty', () => <TaskList tasks={[]} {...actions} />);
+  .add('default', () => <PureTaskList tasks={defaultTasks} {...actions} />)
+  .add('withPinnedTasks', () => <PureTaskList tasks={withPinnedTasks} {...actions} />)
+  .add('loading', () => <PureTaskList loading tasks={[]} {...actions} />)
+  .add('empty', () => <PureTaskList tasks={[]} {...actions} />);
